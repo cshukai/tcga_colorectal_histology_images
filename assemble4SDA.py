413c99e8811d0d151463d5797640bdc6947032de
cshukai <cshukai@gmail.com>
2019-03-30 21:19:43 -0500

    bugfix

--- assemble4SDA.py
+++ assemble4SDA.py
@@ -18,7 +18,8 @@ totalsize=100 # number of final patches of one image for learning
 
 edge_noise_patch_num=int(math.ceil(int(math.sqrt(totalsize))*edge_noise_ratio))
 inner_noise_patch_num=int(math.ceil(totalsize*inner_noise_ration))
-
+print(edge_noise_patch_num)
+print(inner_noise_patch_num)
 
 def rowWiseImgCombine(rowArr,tiffs):
     pathArr=map(lambda idx: tiffs[idx],rowArr)
@@ -52,7 +53,7 @@ def index2Pixels(i,label_row_col_indices,unitPixel):
                 y_pixels=range(y_pixel_start,y_pixel_end+1)
                 all_points_x=all_points_x+x_pixels
                 all_points_y=all_points_y+y_pixels 
-            result[i]={'filename':str(i)+'tif','name':tiss,'regions':{'all_points_x':all_points_x,'all_points_y':all_points_y} }
+            result[i]={'filename':str(i)+'.tif','name':tiss,'regions':{'all_points_x':all_points_x,'all_points_y':all_points_y} }
     return(result)        
     
 def getIndexForLabel(label,labelArr):
@@ -76,23 +77,36 @@ for j,path in enumerate(tissuelist):
     
     this_tissue=path.split("/")[1]
     this_data_source=path.split("/")[0]
-    noisy_tiflist=glob(this_data_source+"/"+"[!"+this_tissue+"]*"+'/*.tif')
-
+    noisy_folder=list((s for s in tissuelist if this_tissue.lower() not in s.lower()))
+    noisy_tiflist=map(lambda tis:glob(tis+"/*.tif"),noisy_folder)
+    noisy_tiflist = [item for sublist in noisy_tiflist for item in sublist]
     for i in range(total_num):
         noisyTiffs=random.sample(noisy_tiflist,edge_noise_patch_num+inner_noise_patch_num)
-        homoTiffs=random.sample(homo_tiflist,totalsize-(edge_noise_patch_num+inner_noise_patch_num))
-        totalTiffs=noisyTiffs+homoTiffs
-        random.shuffle(totalTiffs)
-        unit=int(math.sqrt(totalsize))
-        
-        idices=np.reshape(range(totalsize),(unit,unit))
-        labelArrs=map(lambda row:getRowWiseLabels(row,totalTiffs),idices)
-        rowResults=map(lambda row: rowWiseImgCombine(row,totalTiffs),idices)
-        finalImg=reduce(lambda rowImg1,rowImg2:np.hstack((rowImg1,rowImg2)),rowResults)
-        unitPixel=finalImg.shape[0]/unit
-        ann=getAnoDictionary(i,labelArrs,unitPixel)
-        finalAnn.update(ann)
-        tiff.imsave(this_tissue+"_"+str(i)+'.tif',finalImg)
+        origTiffs=random.sample(homo_tiflist,totalsize)
 
-    with open(this_tissue+"_"+'annotation.json', 'w') as outfile:
-        json.dump(finalAnn, outfile)
+        
+        unit=int(math.sqrt(totalsize))
+        idices=np.reshape(range(totalsize),(unit,unit))
+        
+        #authentic
+        labelArrs=map(lambda row:getRowWiseLabels(row,origTiffs),idices)
+        rowResults=map(lambda row: rowWiseImgCombine(row,origTiffs),idices)
+        finalImg=reduce(lambda rowImg1,rowImg2:np.hstack((rowImg1,rowImg2)),rowResults)
+        tiff.imsave(this_tissue+"_orig"+"_"+str(i)+'.tif',finalImg)
+        #add noise to authentic image
+        x_edges=random.sample(range((unit-1)*224-1),edge_noise_patch_num)
+        y_edges=random.sample(range(223)+range((unit-2)*224-1,(unit-1)*224-1),edge_noise_patch_num)
+        x_inner=random.sample(range(224,(unit-2)*224-1),inner_noise_patch_num)
+        y_inner=random.sample(range(224,(unit-2)*224-1),inner_noise_patch_num)
+        x_start=x_edges+x_inner
+        y_start=y_edges+y_inner
+        annotation=pd.DataFrame(columns=['noise_type', 'x', 'y'])
+        for k,noisy_path in enumerate(noisyTiffs):
+            this_noisy_img=cv2.imread(noisy_path,-1)
+            this_noisy_tiss=noisy_path.split("/")[1]
+            finalImg[x_start[k]:x_start[k]+224,y_start[k]:y_start[k]+224]=this_noisy_img
+            annotation=annotation.append({'noise_type': this_noisy_tiss, 'x':x_start[k],'y':y_start[k]}, ignore_index=True)
+        tiff.imsave(this_tissue+"_noisy"+"_"+str(i)+".tif",finalImg)
+        annotation.to_csv(this_tissue+"_"+str(i)+".csv")
+        
+        
