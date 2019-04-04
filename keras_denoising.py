a141f8e8d5b74ff617b612d5216673a160665701
cshukai <cshukai@gmail.com>
2019-04-04 15:59:57 -0500

    add nonlinearity

--- keras_denoising.py
+++ keras_denoising.py
@@ -13,7 +13,7 @@ set_session(sess)  # set this TensorFlow session as the default session for Kera
 
 import keras
 from keras.models import Model
-from keras.layers import Conv2D, MaxPooling2D, Dense, Input, Reshape, Flatten, Lambda, Conv2DTranspose 
+from keras.layers import Conv2D, MaxPooling2D, Dense, Input, Reshape, Flatten, Lambda, Conv2DTranspose,Activation 
 from keras import backend as K
 #import matplotlib.pyplot as plt
 import numpy as np
@@ -31,8 +31,8 @@ labels=os.listdir()
 img_num=100 #per class ; train one denoising autoencoder for each class
 width=2240
 height=2240
-channel=3
-train_size=0.8
+channel=1
+train_size=80
 train_num=int(math.ceil(img_num*0.8))
 test_num=img_num-train_num
 ################################################
@@ -46,37 +46,40 @@ for i,lbl in enumerate(labels):
     for j in range(train_num):
         this_noise_path=lbl+"/"+lbl+"_"+"noisy"+"_"+str(j)+".tif"
         this_orig_path=lbl+"/"+lbl+"_"+"orig"+"_"+str(j)+".tif"
-        X_train[j]=cv2.imread(this_noise_path,-1)/255
-        X_orig[j]=cv2.imread(this_orig_path,-1)/255
+        X_train[j]=np.reshape(cv2.imread(this_noise_path,cv2.IMREAD_GRAYSCALE)/255,(width,height,channel))
+        X_orig[j]=np.reshape(cv2.imread(this_orig_path,cv2.IMREAD_GRAYSCALE)/255,(width,height,channel))
         
     # creating denoising autoencoder model
-    inputs = Input(shape = (2240,2240,3))
+    inputs = Input(shape = (width,height,channel))
     
     conv1 = Conv2D(16, (3,3), activation = 'relu', padding = "SAME")(inputs)
     pool1 = MaxPooling2D(pool_size = (2,2), strides = 2)(conv1)
-    conv2 = Conv2D(32, (3,3), activation = 'relu', padding = "SAME")(pool1)
+    act1 = Activation('relu')(pool1)
+    conv2 = Conv2D(32, (3,3), activation = 'relu', padding = "SAME")(act1)
     pool2 = MaxPooling2D(pool_size = (2,2), strides = 2)(conv2)
-    
-    upsampling_1 = Conv2DTranspose(32, 3, padding='same', activation='relu', strides=(2, 2))(pool2)
-    upsampling_2 = Conv2DTranspose(16, 3, padding='same', activation='relu', strides=(2, 2))(upsampling_1)
-    outputs = Conv2DTranspose(3, 3, padding='same', activation='relu')(upsampling_2)
+    act2=Activation('relu')(pool2)
+    upsampling_1 = Conv2DTranspose(32, 3, padding='same', activation='relu', strides=(2, 2))(act2)
+    act3=Activation('relu')(upsampling_1)
+    upsampling_2 = Conv2DTranspose(16, 3, padding='same', activation='relu', strides=(2, 2))(act3)
+    outputs = Conv2DTranspose(channel, 3, padding='same', activation='relu')(upsampling_2)
     autoencoder = Model(inputs, outputs)
     m = 1
-    n_epoch = 100
+    n_epoch = 1
     autoencoder.compile(optimizer='sgd', loss='binary_crossentropy')
-    autoencoder.fit(X_train,X_orig, epochs=n_epoch, batch_size=m, shuffle=True)
-        
-    X_test=np.zeros(shape=(test_num,width,height,channel),dtype=np.float64)
+    autoencoder.fit(X_train,X_orig, epochs=n_epoch, batch_size=m, shuffle=True,validation_split=0.2)
+    print("predicting")    
+    X_test=np.zeros(shape=(test_num,width,height,3),dtype=np.float64)
     for r in range(test_num):
         num=train_num+r
-        path=lbl+"/"+lbl+"_"+"orig"+"_"+str(num)+".tif"
-        this_test_img=cv2.imread(path,-1)
+        path=lbl+"/"+lbl+"_"+"noisy"+"_"+str(num)+".tif"
+        this_test_img=cv2.imread(path,cv2.IMREAD_GRAYSCALE)
         X_test[r]=this_test_img
     
     decoded_imgs = autoencoder.predict(X_test,batch_size=1)
     diff_imgs=X_test-decoded_imgs
 
     for r in range(test_num):
+        print("writing")
         num=train_num+r
         this_decoded_name=lbl+"/"+lbl+"_"+"dec"+"_"+str(num)+".tif"
         this_diff_name=lbl+"/"+lbl+"_"+"dif"+"_"+str(num)+".tif"
