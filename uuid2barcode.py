0650e74e983420188c70cca7973a278537464a90
Shu-Kai Chang <cshukai@gmail.com>
2019-07-13 23:36:33 -0500

    Update uuid2barcode.py

--- uuid2barcode.py
+++ uuid2barcode.py
@@ -1,25 +1,37 @@
-#python2
-#import requests
 import json
 import os
 import pandas
 import grequests
+import asyncio
 
-
-
-uuids=os.listdir('coad')
+loop = asyncio.get_event_loop()
+uuids=os.listdir('/storage/htc/nih-tcga/wholeslides/coad')
 file_endpt = 'https://api.gdc.cancer.gov/legacy/files/'
+
+def getAgainIfstillMissing(x):
+     l=[i for i,v in enumerate(x) if v == None]
+     if len(l)>0:
+       urlss = [urls[i] for i in l]
+       loop.call_later(5,fireBulkRequest(urlss))
+
+def fireBulkRequest(urls):
+    rs = (grequests.get(u,timeout=100) for u in urls)
+    x=grequests.map(rs)
+    loop.call_later(5,getAgainIfstillMissing(x))
+
+if __name__ == '__main__':
+    loop.call_soon(fireBulkRequest)
+    loop.run_forever()
+
+    
 df=pandas.DataFrame(columns=['uuid', 'barcode'])
 
 urls=[]
 for i,uuid in enumerate(uuids) :
     urls.append(file_endpt + uuid)
 
-rs = (grequests.get(u) for u in urls)
-x=grequests.map(rs) # x is a list
-nonetype_idx=
-#        x=response.json()
-#        y=x['data']['file_name']
-#        barcode="-".join(y.split("-")[0:3])
-#        df.loc[i]=uuid+barcode
+
+rs = (grequests.get(u,timeout=10) for u in urls)
+grequests.map(rs)
+
 df.to_csv(path="coad.barcode.csv")
