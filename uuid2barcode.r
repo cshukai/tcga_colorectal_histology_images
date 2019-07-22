b05b799378a9c0df18333d3ad3281f68616c6602
Shu-Kai Chang <cshukai@gmail.com>
2019-07-21 20:58:25 -0500

    Update uuid2barcode.r

--- uuid2barcode.r
+++ uuid2barcode.r
@@ -10,5 +10,15 @@ BiocManager::install("TCGAutils")
 source("https://bioconductor.org/biocLite.R")
 biocLite("TCGAutils")
 ##########################Process##########################
-
+setwd("manifest/")
+library("TCGAutils")
+files=dir()
+# UUID tbl -> barcod list
+result=list()
+for(i in 1:length(files)){
+    temp=unlist(strsplit(x=files[i],split="\\."))
+    this_type=temp[2]
+    d=read.table(files[i],sep="\t",header=T)
+    
+}
 UUIDtoBarcode("c5354db6-6d87-46e4-8e10-d1493e7226a7", id_type = "file_id")
