c4f9016f8fa8b52cc8291c9b45bcb7f63ceab987
Shu-Kai Chang <cshukai@gmail.com>
2019-07-22 22:59:54 -0500

    Update classification.py

--- preporcessing/vgg19/classification.py
+++ preporcessing/vgg19/classification.py
@@ -1,4 +1,6 @@
 import scipy.io
+from keras.applications.vgg19 import VGG19
 
 x=x=scipy.io.loadmat("/home/shchang/scratch/whole_slide_patches/deep-stroma-histology/lastNet_TEXTURE_VGG.mat")
-weight=x['None'][0][3]
+weights=x['None'][0][3]
+model= VGG19(weights=weights)
