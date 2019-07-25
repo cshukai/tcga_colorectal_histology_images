320bb761ee90b7ccdcc4dcfc3076982a8fdbdea7
Shu-Kai Chang <cshukai@gmail.com>
2019-07-25 16:59:55 -0500

    Update classification.py

--- preporcessing/vgg19/classification.py
+++ preporcessing/vgg19/classification.py
@@ -1,6 +1,11 @@
-import scipy.io
+import os
 from keras.applications.vgg19 import VGG19
+from keras.preprocessing import image
+from keras.applications.vgg19 import preprocess_input
+from keras.models import Model
+import numpy as np
+# input
 
-x=x=scipy.io.loadmat("/storage/htc/nih-tcga/sc724/tcga_current/whole_slide_patches/deep-stroma-histology/lastNet_TEXTURE_VGG.mat")
-#weights are here x['None'][0][3]
-model= VGG19(weights=weights)
+# normalization
+# training
+model= VGG19(weights='imagenet')
