4b7375474ae7077a3d5de4656319679f727dc8ad
Shu-Kai Chang <cshukai@gmail.com>
2019-07-27 19:46:27 -0500

    Update svs2tiff.m

--- preprocessing/tcga/svs2tiff.m
+++ preprocessing/tcga/svs2tiff.m
@@ -1,1 +1,9 @@
-x=dir ('/storage/htc/nih-tcga/sc724/tcga_current/coad/img_wholeslide/*/*.svs')
+deploy_dataPath = '/storage/htc/nih-tcga/sc724/tcga_current/coad/exp/slide';
+allFiles = dir([deploy_dataPath,'*.svs']);
+
+for i = 1:numel(allFiles)
+  currFilePath = [deploy_dataPath,allFiles(i).name];
+  disp(currFilePath)
+  I=imread(curFilePath,'Index',1)
+  
+  
