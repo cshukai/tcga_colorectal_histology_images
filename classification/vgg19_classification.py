d30bacaea87453aaecdfe11070cb5491bdf2a086
Shu-Kai Chang <cshukai@gmail.com>
2019-07-31 17:09:04 -0500

    input already normalized added checkpoint

--- classification/vgg19_classification.py
+++ classification/vgg19_classification.py
@@ -5,20 +5,26 @@ from keras.applications.vgg19 import preprocess_input
 from keras.models import Model
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras import optimizers
+from keras.callbacks import ModelCheckpoint
+
 import numpy as np
 
 # config
+
 batch_size = 32
 
 # input
-train_datagen = ImageDataGenerator(rescale=1./255,shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
+train_datagen = ImageDataGenerator(shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
 
-train_generator = train_datagen.flow_from_directory('/storage/htc/nih-tcga/sc724/tcga_current/whole_slide_patches/images/wholeslide/colorectal/NCT-CRC-HE-100K', 
+train_generator = train_datagen.flow_from_directory('/storage/htc/nih-tcga/sc724/tcga_current/whole_slide_patches/images/wholeslide/colorectal/NCT-CRC-HE-10
+0K', 
         target_size=(224, 224),  # all images will be resized to 150x150
         batch_size=batch_size)   # categorical for 1 hot encoding is default
 
-test_datagen = ImageDataGenerator(rescale=1./255)
-test_generator = test_datagen.flow_from_directory('/storage/htc/nih-tcga/sc724/tcga_current/whole_slide_patches/images/wholeslide/colorectal/CRC-VAL-HE-7K', 
+test_datagen = ImageDataGenerator()
+test_generator = test_datagen.flow_from_directory('/storage/htc/nih-tcga/sc724/tcga_current/whole_slide_patches/images/wholeslide/colorectal/CRC-VAL-HE-7K',
+ 
         target_size=(224, 224),  # all images will be resized to 150x150
         batch_size=batch_size) 
 
@@ -29,15 +35,14 @@ model= VGG19(weights=None,classes=9)
 
 # training
 
-
+checkpointer = ModelCheckpoint(filepath='/storage/htc/nih-tcga/sc724/300epoch_checkpoint.hdf5', verbose=1, save_best_only=True)
 model.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
-              metrics=['accuracy'])
+              optimizer=optimizers.SGD(lr=3e-6, momentum=0.9),
+              metrics=['acc'])
 model.fit_generator(
         train_generator,
         steps_per_epoch=2000 // batch_size,
-        epochs=15,
+        epochs=300,
         validation_data=test_generator,
-        validation_steps=800 // batch_size)
-
-model.save_weights('first_try.h5')
+        validation_steps=800 // batch_size,
+        callbacks=[checkpointer])
