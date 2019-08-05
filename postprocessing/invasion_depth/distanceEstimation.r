5b0410dca4df74dc878184470fc6f6fecd8b1394
Shu-Kai Chang <cshukai@gmail.com>
2019-08-04 22:17:25 -0500

    patch center done

--- postprocessing/invasion_depth/distanceEstimation.r
+++ postprocessing/invasion_depth/distanceEstimation.r
@@ -9,7 +9,11 @@ for(i in 1:nrow(patch_list)){
  temp=unlist(strsplit(x=as.character(patch_list[i,1]),split="/"))
  locs=temp[length(temp)]
  temp2=unlist(strsplit(x=locs,split="_"))
- this_width=as.integer(gsub(x=temp2[3],pattern=".tif",replacement=""))
- this_height=as.integer(temp2[2])
- 
+ this_x_start=as.integer(gsub(x=temp2[3],pattern=".tif",replacement=""))
+ this_y_start=as.integer(temp2[2])
+ this_x_end=this_x_start+crop_size
+ this_y_end=this_y_start+crop_size
+ this_patch_center_x=(this_x_start+this_x_end)/2
+ this_patch_center_y=(this_y_start+this_y_end)/2
+                        
 }
