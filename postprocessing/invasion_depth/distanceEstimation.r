f33b22330cd55da3213b48ec395c2defbe8a86f4
Shu-Kai Chang <cshukai@gmail.com>
2019-08-07 17:49:54 -0500

    Update distanceEstimation.r

--- postprocessing/invasion_depth/distanceEstimation.r
+++ postprocessing/invasion_depth/distanceEstimation.r
@@ -23,9 +23,9 @@ unique(allpatient)
 
 
 ##########################invasion depth ###################
+result=NULL
 barcode_list=unique(barcodes) 
 crop_size=225
-eu_dists=NULL
 for(j in 1:length(barcode_list)){
   this_patch_list=patch_list[grep(x=patch_list[,1],pattern=barcode_list[j]),]
   xs=NULL
@@ -48,6 +48,25 @@ for(j in 1:length(barcode_list)){
   left_boundary=min(xs)
   right_boundary=max(xs)
   up_bounary=min(ys)
-  right_boundary=max(ys)
+  bottom_boundary=max(ys)
+  center_x=(left_boundary+right_boundary)/2
+  center_y=(up_bounary+bottom_boundary)/2
+  
+  eu_dists=NULL
+  for(i in 1:nrow(this_patch_list)){
+    temp=unlist(strsplit(x=as.character(this_patch_list[i,1]),split="/"))
+    locs=temp[length(temp)]  
+    temp2=unlist(strsplit(x=locs,split="_"))
+    this_x_start=as.integer(gsub(x=temp2[3],pattern=".tif",replacement=""))
+    this_y_start=as.integer(temp2[2])
+    this_x_end=this_x_start+crop_size
+    this_y_end=this_y_start+crop_size
+    this_patch_center_x=(this_x_start+this_x_end)/2
+    this_patch_center_y=(this_y_start+this_y_end)/2
+    this_dist=eudist(this_patch_center_x,center_x,this_patch_center_y,center_y)
+    eu_dists=c(eu_dists,this_dist)
+  }
+  this_patch_list=cbind(this_patch_list,eu_dists)
+  result=rbind(result,this_patch_list)
 }
 
