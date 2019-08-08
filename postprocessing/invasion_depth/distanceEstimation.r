9c7c6ae4cf4f155d909a504cbb13c2d43ca45d44
Shu-Kai Chang <cshukai@gmail.com>
2019-08-07 22:51:42 -0500

    added muscle and lym

--- postprocessing/invasion_depth/distanceEstimation.r
+++ postprocessing/invasion_depth/distanceEstimation.r
@@ -81,7 +81,7 @@ for(j in 1:length(barcode_list)){
 # depth estimation
 for(j in 1:length(barcode_list)){
   this_patch_list=result[grep(x=result[,1],pattern=barcode_list[j]),]
-  this_patch_list=this_patch_list[union(which(this_patch_list[,2]=="MUC"),which(this_patch_list[,2]=="TUM")),]   
+  this_patch_list=this_patch_list[union(union(union(which(this_patch_list[,2]=="MUC"),which(this_patch_list[,2]=="TUM")),which(this_patch_list[,2]=="MUS")),which(this_patch_list[,2]=="LYM")),]   
   colnames(this_patch_list)=c("path","type","distance")
   ggplot(this_patch_list , aes(x=distance, fill=factor(type))) + geom_histogram()
 }
