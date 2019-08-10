a7747b2ebe2fbf8ca08d958b37d24018dd440abc
Shu-Kai Chang <cshukai@gmail.com>
2019-08-10 14:59:39 -0500

    just looking at T and N at this point

--- preprocessing/tcga/staging.r
+++ preprocessing/tcga/staging.r
@@ -1,3 +1,4 @@
+######################staging information processing#####
 clic=read.csv("COAD_clinical_patients.csv",header=T)
 t_class=NULL
 n_class=NULL
@@ -6,6 +7,9 @@ for(i in 1:nrow(clic)){ #not looking at metastasis at this point
   temp=unlist(strsplit(x=as.character(clic[i,"stage_event_tnm_categories"]),split=""))
   this_T=paste(temp[2:(grep(x=temp,pattern="N")-1)],collapse="")
   this_N=paste(temp[(grep(x=temp,pattern="N")+1):length(temp)],collapse="")
+  if(length(grep(x=temp,pattern="M"))>0){
+       this_N=substring(text=this_N,first=1,last=(regexpr(text=this_N,pattern="M")-1))
+  }
   #this_M=temp[(grep(x=temp,pattern="M")+1):length(temp)]
   t_class=c(t_class,this_T)
   n_class=c(n_class, this_N)
