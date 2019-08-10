c0eea9b0fe858aa793a08294b310d5e919be6301
Shu-Kai Chang <cshukai@gmail.com>
2019-08-10 12:53:27 -0500

    Create staging.r

---
+++ preprocessing/tcga/staging.r
@@ -0,0 +1,13 @@
+clic=read.csv("COAD_clinical_patients.csv",header=T)
+t_class=NULL
+n_class=NULL
+#m_class=NULL
+for(i in 1:nrow(clic)){ #not looking at metastasis at this point
+  temp=unlist(strsplit(x=as.character(clic[i,"stage_event_tnm_categories"]),split=""))
+  this_T=paste(temp[2:(grep(x=temp,pattern="N")-1)],collapse="")
+  this_N=paste(temp[(grep(x=temp,pattern="N")+1):length(temp)],collapse="")
+  #this_M=temp[(grep(x=temp,pattern="M")+1):length(temp)]
+  t_class=c(t_class,this_T)
+  n_class=c(n_class, this_N)
+  #m_class=c(m_class,this_M)
+}
